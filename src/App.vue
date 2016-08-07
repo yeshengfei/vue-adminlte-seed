cdc70df9f675694fd014545687df22d089a3b3bb
yeshengfei <191705595@qq.com>
2016-08-07 16:38:52 +0800

    adminlte的起始代码与sample

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <main-header></main-header>
+    <main-sidebar></main-sidebar>
+    <div class="content-wrapper">
+      <hello></hello>
+    </div>
+    <main-footer></main-footer>
+    <control-sidebar></control-sidebar>
+    <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import MainHeader from './components/common/MainHeader'
+import MainSidebar from './components/common/MainSidebar'
+import MainFooter from './components/common/MainFooter'
+import ControlSidebar from './components/common/ControlSidebar'
+
+export default {
+  components: {
+    Hello,
+    MainHeader,
+    MainSidebar,
+    MainFooter,
+    ControlSidebar
+  }
+}
+</script>
+
+<style lang="less">
+  .wrapper {
+    min-height: 600px;
+  }
+
+</style>
---
+++ src/components/common/MainFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.3.5
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+      name : 'main-footer'
+    }
+</script>
